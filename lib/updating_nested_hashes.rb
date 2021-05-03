614251ada680ee4e9d498764a0d23209b60fb7eb
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-05-03 11:47:51 +1000

    Removed function call and print call

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -66,9 +66,7 @@ def add_characters
    epic_tragedy[:additional_characters] = []
    epic_tragedy[:additional_characters][0] = {name: "Prince Escalus"} 
    epic_tragedy[:additional_characters][1] = {name: "Apothecary"}
-   pp epic_tragedy
+
    # After your code, the following line will return the altered hash
    epic_tragedy
 end
-
-add_characters
